8c0ba91f3b770e6247edab259aec76d57e8ce95f
Vinicius Herminio Gama da Silva <viniciusherminio1@outlook.com>
2022-11-29 17:36:36 -0300

    requisito 4 concluido

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Login() {
+  const [disabled, setDisabled] = useState(true);
+
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  });
+
+  const validation = () => {
+    const SIX = 7;
+    const regex = /[\w.ã]+@\w+\.\w{2,8}(\.\w{0,2})?/g;
+    const validEmail = regex.test(login.email);
+    if (login.password.length >= SIX && validEmail) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+
+  useEffect(() => {
+    validation();
+  }, [login.password]);
+
+  const handleChange = ({ target }) => {
+    const { value, name } = target;
+    if (name === 'email') {
+      setLogin(() => ({
+        ...login,
+        email: value,
+      }));
+    } if (name === 'password') {
+      setLogin(() => ({
+        ...login,
+        password: value,
+      }));
+    }
+    validation();
+  };
+
   return (
     <div>
       <input
         data-testid="email-input"
         placeholder="Email"
+        value={ login.email }
+        onChange={ handleChange }
+        name="email"
+        type="email"
       />
       <input
         data-testid="password-input"
         placeholder="Senha"
+        name="password"
+        value={ login.password }
+        type="password"
+        onChange={ handleChange }
       />
       <button
         data-testid="login-submit-btn"
         type="button"
+        disabled={ disabled }
       >
         Enter
 

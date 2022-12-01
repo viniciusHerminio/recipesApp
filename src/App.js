83c6d1af924c1dd36879e695af2042c0f4d7663e
Mirella Dourado <100709341+MirellaDourado@users.noreply.github.com>
2022-12-01 15:52:18 -0300

    Fixing routes

--- src/App.js
+++ src/App.js
@@ -13,18 +13,49 @@ import RecipeDetails from './pages/RecipeDetails';
 function App() {
   return (
     <Switch>
-      <Route exact path="/" render={ () => <Login /> } />
-      <Route path="/profile" render={ () => <Profile /> } />
-      <Route path="/meals" render={ () => <Recipes type="meals" /> } />
-      <Route path="/drinks" render={ () => <Recipes type="drinks" /> } />
-      <Route exact path="/done-recipes" render={ () => <DoneRecipes /> } />
-      <Route exact path="/favorite-recipes" render={ () => <FavoriteRecipes /> } />
-      <Route exact path="/meals/:id" render={ (props) => <RecipeDetails
-      {...props } type="meals" /> } />
-      <Route exact path="/drinks/:id" render={ (props) => <RecipeDetails
-      { ...props } type="drinks" /> } />
-      <Route exact path="/meals/:id/in-progress" render={ () => <RecipeInProgress /> } />
-      <Route exact path="/drinks/:id/in-progress" render={ () => <RecipeInProgress /> } />
+      <Route
+        exact
+        path="/"
+        render={ () => <Login /> }
+      />
+      <Route
+        path="/profile"
+        render={ () => <Profile /> }
+      />
+      <Route
+        exact
+        path="/meals"
+        render={ () => <Recipes type="meals" /> }
+      />
+      <Route
+        exact
+        path="/drinks"
+        render={ () => <Recipes type="drinks" /> }
+      />
+      <Route 
+        path="/done-recipes"
+        render={ () => <DoneRecipes /> }
+      />
+      <Route 
+        path="/favorite-recipes"
+        render={ () => <FavoriteRecipes /> }
+      />
+      <Route
+        path="/meals/:id"
+        render={ (props) => <RecipeDetails {...props } type="meals" /> }
+      />
+      <Route
+        path="/drinks/:id"
+        render={ (props) => <RecipeDetails { ...props } type="drinks" /> }
+      />
+      <Route
+        path="/meals/:id/in-progress"
+        render={ () => <RecipeInProgress /> }
+      />
+      <Route
+        path="/drinks/:id/in-progress"
+        render={ () => <RecipeInProgress /> }
+      />
     </Switch>
   );
 }

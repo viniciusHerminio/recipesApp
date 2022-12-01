2cd8e58644ff3dd821237f12184e791719994c10
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-12-01 18:13:27 -0300

    Criando o context e o provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import RecipesAppProvider from './context/RecipesAppProvider';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <BrowserRouter>
-      <App />
+      <RecipesAppProvider>
+        <App />
+      </RecipesAppProvider>
+      ,
     </BrowserRouter>,
   );
 

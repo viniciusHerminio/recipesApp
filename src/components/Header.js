11d08a112f5c56030267b3b6b831a01bf407dd9c
Josimar <josimarsaldanha@gmail.com>
2022-12-14 22:33:05 -0300

    style: header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Header.css';
 import iconProfile from '../images/profileIcon.svg';
 import iconSearch from '../images/searchIcon.svg';
+import recipesIcon from '../images/iconRecipesApp.png';
+import recipesName from '../images/nameRecipesApp.png';
+import foodsIcon from '../images/icone-prato.png';
+import drinksIcon from '../images/icone-bebida.png';
 import RecipesAppContext from '../context/RecipesAppContext';
 
 function Header({ search, profileUser, children }) {
@@ -9,22 +14,41 @@ function Header({ search, profileUser, children }) {
   const { setSearchInput } = useContext(RecipesAppContext);
   return (
     <div>
-      {profileUser && (
-        <Link to="/profile">
-          <img alt="iconProfile" src={ iconProfile } data-testid="profile-top-btn" />
-        </Link>
-      )}
-      {search && (
-        <button onClick={ () => setSearching((previoStat) => !previoStat) } type="button">
-          <img alt="iconSearch" src={ iconSearch } data-testid="search-top-btn" />
-        </button>
-      )}
-      {searching && <input
-        type="text"
-        data-testid="search-input"
-        onChange={ ({ target: { value } }) => setSearchInput(value) }
-      />}
-      <p data-testid="page-title">{children}</p>
+      <div className="top-header">
+        <div className="top-images">
+          <img src={ recipesIcon } alt="logo" />
+          <img src={ recipesName } alt="Recipes App" />
+        </div>
+        <div>
+          {profileUser && (
+            <Link to="/profile">
+              <img alt="iconProfile" src={ iconProfile } data-testid="profile-top-btn" />
+            </Link>
+          )}
+          {search && (
+            <button
+              className="search-top-btn"
+              onClick={ () => setSearching((previoStat) => !previoStat) }
+              type="button"
+            >
+              <img alt="iconSearch" src={ iconSearch } data-testid="search-top-btn" />
+            </button>
+          )}
+          {searching && <input
+            type="text"
+            data-testid="search-input"
+            onChange={ ({ target: { value } }) => setSearchInput(value) }
+          />}
+        </div>
+      </div>
+      <div className="top-type">
+        <img
+          className="img-type"
+          src={ children === 'Meals' ? foodsIcon : drinksIcon }
+          alt="type"
+        />
+        <p className="type-text" data-testid="page-title">{children}</p>
+      </div>
     </div>
   );
 }
